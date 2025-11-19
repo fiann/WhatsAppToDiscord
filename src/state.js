00f05c8d0e777846f9464fe707ea9d9690b9ffc7
arespawn <sniperman_koukou@hotmail.com>
2025-11-19 14:53:21 +0200

    fix state exports for tests

--- src/state.js
+++ src/state.js
@@ -58,4 +58,19 @@ const state = {
   version: '',
 };
 
+export const settings = state.settings;
+export const dcClient = () => state.dcClient;
+export const waClient = () => state.waClient;
+export const chats = state.chats;
+export const contacts = state.contacts;
+export const startTime = () => state.startTime;
+export const logger = () => state.logger;
+export const lastMessages = () => state.lastMessages;
+export const sentMessages = state.sentMessages;
+export const reactions = state.reactions;
+export const sentReactions = state.sentReactions;
+export const goccRuns = state.goccRuns;
+export const updateInfo = () => state.updateInfo;
+export const version = () => state.version;
+
 export default state;
