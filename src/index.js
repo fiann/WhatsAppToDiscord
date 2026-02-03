d49eda85f8dbd1946b021a17003910824847b3a6
arespawn <26845951+arespawn@users.noreply.github.com>
2026-02-03 12:19:00 +0200

    chore: bump version to beta

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ if (!globalThis.crypto) {
 }
 
 (async () => {
-  const version = 'v2.1.3';
+  const version = 'v2.1.4-beta.1';
   state.version = version;
   const streams = [
     { stream: pino.destination('logs.txt') },
